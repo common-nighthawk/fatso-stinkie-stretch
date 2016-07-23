89f371460994a4a2eb2dbcc900099a1d7a191759
Daniel <danielallendeutsch@gmail.com>
2016-07-23 11:03:46 -0500

    adds filters for scope and warehouse to the inventories#index view.

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,23 +1,18 @@
 class InventoriesController < ApplicationController
   def index
     @inventories = Inventory.send(sanitized_scope)
-    @inventories = @inventories.where(warehouse: sanitized_warehouse) if params['warehouse']
+    @inventories = @inventories.where(warehouse: sanitized_warehouse) if params[:warehouse]
   end
 
   private
 
   def sanitized_scope
-    if params['shipped']
-      :shipped
-    elsif params['not_shipped']
-      :not_shipped
-    else
-      :all
-    end
+    shipped_param = params[:shipped].to_s.downcase
+    %w(shipped not_shipped).include?(shipped_param) ? shipped_param : :all
   end
 
   def sanitized_warehouse
-    name = params['warehouse'].downcase
+    name = params[:warehouse].downcase
     Warehouse.find_by(name: name)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def btn_classes(key=nil, value=nil)
+    additional_class = (value.present? && params[key] == value) ? 'btn-primary' : ''
+    base_classes + additional_class
+  end
+
+  private
+
+  def base_classes
+    'btn btn-default '
+  end
 end
