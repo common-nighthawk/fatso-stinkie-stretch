31df7e6770bc482be7b53debc438f7c683088c15
Daniel <danielallendeutsch@gmail.com>
2016-07-24 22:18:31 -0500

    adds custom validation to assignment to prevent inventory-shipment mismatch in warehouse.

--- app/models/inventory_shipment_assignment.rb
+++ app/models/inventory_shipment_assignment.rb
@@ -6,4 +6,13 @@ class InventoryShipmentAssignment < ActiveRecord::Base
   validates :inventory, presence: true
   validates :shipper, presence: true
   validates :inventory_id, uniqueness: { scope: :shipment_id }
+  validate :inventory_in_shipment_from_same_warehouse
+
+  private
+
+  def inventory_in_shipment_from_same_warehouse
+    if shipment && (inventory.warehouse != shipment.warehouse)
+      errors.add(:inventory, 'must be assigned to shipment from same warehouse')
+    end
+  end
 end
--- spec/models/inventory_shipment_assignment_spec.rb
+++ spec/models/inventory_shipment_assignment_spec.rb
@@ -8,8 +8,21 @@ RSpec.describe InventoryShipmentAssignment, type: :model do
   end
 
   describe 'validations' do
+    let(:product) { Product.create!(name: 'product') }
+    let(:pittsburgh) { Warehouse.create!(name: 'pittsburgh') }
+    let(:minnesota) { Warehouse.create!(name: 'minnesota') }
+    let(:user) { FulfillmentSpecialist.create!(name: 'elizabeth', email: 'elizabeth@example.com', location: pittsburgh) }
+
     it { is_expected.to validate_presence_of(:inventory) }
     it { is_expected.to validate_presence_of(:shipper) }
     it { is_expected.to validate_uniqueness_of(:inventory_id).scoped_to(:shipment_id) }
+
+    it 'cannot assign inventory to shipment from different warehouse' do
+      shipment_from_pittsburg  = Shipment.create!(warehouse: pittsburgh, expected_departure_date: Date.today)
+      inventory_from_minnesota = Inventory.create!(product: product, warehouse: minnesota)
+
+      assignment = InventoryShipmentAssignment.new(inventory: inventory_from_minnesota, shipment: shipment_from_pittsburg, shipper: user)
+      expect(assignment).to_not be_valid
+    end
   end
 end
